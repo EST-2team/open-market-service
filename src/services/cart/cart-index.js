71cb3af24d78b90c1472a17c1752b1adab467aba
gimeast <slasnrndu@gmail.com>
2025-07-27 05:53:48 +0900

    feat: 장바구니 api 로직 수정
    ## 🔨 작업 내용
    - api 호출 함수에서 직접 렌더링이 아닌 데이터를 반환 하도록 수정
    
    ## 💬 기타 참고 사항
    - 없음

--- src/services/cart/cart-index.js
+++ src/services/cart/cart-index.js
@@ -1,8 +1,10 @@
 import { fetchCartList } from "./cart-api.js";
 import { initCartModalListener } from "./modal-service.js";
 import { initCartListener } from "./service.js";
+import { renderCartItem } from "../../components/cart/cart-item.js";
 document.addEventListener("DOMContentLoaded", async () => {
-    await fetchCartList();
+    const cartData = await fetchCartList();
+    renderCartItem(cartData);
 
     initCartListener();
     initCartModalListener();
