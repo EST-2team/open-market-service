f634dc06c74e35e48515a0a32e431339a0509666
gimeast <slasnrndu@gmail.com>
2025-07-27 15:29:28 +0900

    feat: 토큰 재발급하는 customFetch 함수 추가
    ## 🔨 작업 내용
    - 공통으로 사용 할 커스텀 fetch 함수 추가
    
    ## 💬 기타 참고 사항
    - 저희가 공통으로 사용할 fetch 입니다! 버그가 발생 할 만한 부분을 생각해 주세요

---
+++ src/utils/customFetch.js
@@ -0,0 +1,31 @@
+import { getValidAccessToken } from "../services/login/login-token.js";
+
+export async function customFetch(url, options, retryCount = 0) {
+    try {
+        const res = await fetch(url, options);
+
+        if (
+            options.headers?.Authorization &&
+            res.status === 401 &&
+            retryCount < 1 &&
+            retryCount < 3
+        ) {
+            const token = await getValidAccessToken();
+            if (token) {
+                console.log(2);
+                const newOptions = {
+                    ...options,
+                    headers: {
+                        ...options.headers,
+                        Authorization: `Bearer ${token}`,
+                    },
+                };
+                return await customFetch(url, newOptions, retryCount + 1);
+            }
+        }
+        return res;
+    } catch (error) {
+        console.log(error);
+        throw new Error("API 통신 중 오류 발생");
+    }
+}
