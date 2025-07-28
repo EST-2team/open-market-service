f92ee5572c856b32da268b265b94e60c7d3be66c
최진호 <chlwlsgh777@gmail.com>
2025-07-28 18:00:26 +0900

    구매자 판매자 로그인에 따라 마이페이지 판매자센터

--- src/services/logout.js
+++ src/services/logout.js
@@ -1,6 +1,7 @@
 document.querySelector("#logout-btn").addEventListener("click", () => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
+    localStorage.removeItem("userInfo");
     alert("로그아웃 되었습니다.");
-    location.href = "/login.html"; // 로그인 페이지로 이동
+    location.href = "/src/pages/product-list.html";
 });
--- src/services/nav/nav-loader.js
+++ src/services/nav/nav-loader.js
@@ -9,7 +9,33 @@ fetch("/src/pages/nav/header-nav.html")
     })
     .then((html) => {
         navPlaceholder.innerHTML = html;
-        initSearch(); // nav 삽입 완료 후 검색 초기화 함수 실행
+        initSearch();
+
+        const userInfo = localStorage.getItem("userInfo");
+        const userNav = navPlaceholder.querySelector(".login");
+
+        if (userInfo && userNav) {
+            const user = JSON.parse(userInfo);
+
+            if (user.user_type === "SELLER") {
+                userNav.innerHTML = `
+          <div class="mypage">
+            <a href="/src/pages/mypage.html" class="mypage-link" style= "cursor: pointer;">
+              <img src="/src/assets/icons/seller-center-button.svg" alt="판매자 센터" class="seller-center" style="width: 168px; height: 54px"/>
+            </a>
+          </div>
+        `;
+            } else {
+                userNav.innerHTML = `
+          <div class="mypage">
+            <a href="/src/pages/mypage.html" class="login-link">
+              <span class="icon-user"></span>
+              <span class="label" style="font-size: 12px; color: #767676;" >마이페이지</span>
+            </a>
+          </div>
+        `;
+            }
+        }
     })
     .catch((err) => {
         navPlaceholder.innerHTML = "<p>네비게이션 로드 실패</p>";
