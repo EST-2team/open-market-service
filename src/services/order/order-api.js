593def1dc4081f7c96a19088f953a9148d5138f2
gimeast <slasnrndu@gmail.com>
2025-07-28 21:25:21 +0900

    feat: 주문/결제하기
    ## 🔨 작업 내용
    - 최종결제 정보 렌더링
    - input 검증
    - 결제하기 api 연동
    
    ## 💬 기타 참고 사항
    - 없음

--- src/services/order/order-api.js
+++ src/services/order/order-api.js
@@ -13,19 +13,22 @@ async function createOrder(order) {
                 order_type: order.orderType,
                 cart_items: order.cartItems,
                 total_price: order.totalPrice,
-                reciever: order.reciever,
-                reciever_phone_number: order.receiverPhoneNumber,
+                receiver: order.receiver,
+                receiver_phone_number: order.receiverPhoneNumber,
                 address: order.address,
-                address_message: order.message,
+                address_message: order.addressMessage,
                 payment_method: order.paymentMethod,
             }),
         };
         const res = await fetch(url, options);
-        console.log(res);
 
         if (!res.ok) {
+            const error = await res.json();
+            console.error(error);
             throw new Error("주문 생성 중 오류가 발생하였습니다.");
         }
+
+        location.replace("/src/pages/product-list.html");
     } catch (error) {
         console.error(error);
         alert(error);
