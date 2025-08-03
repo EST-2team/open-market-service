28d920343f021ab2fdedf332c23e307a7cf48c6d
gyeone <gyeone@fecsen.com>
2025-08-03 21:20:07 +0900

    배너 자동 슬라이드, 상세 페이지 이동, 배너 이전 다음 버튼 기능 구현

--- src/components/product-list/product-banner.js
+++ src/components/product-list/product-banner.js
@@ -1,12 +1,66 @@
+const $banner = document.querySelector(".banner");
 const $prevBtn = document.querySelector(".banner__prev-btn");
+const $nextBtn = document.querySelector(".banner__next-btn");
+const $bannerSlides = document.querySelector(".banner-slides");
 const $bannerSlide = document.querySelectorAll(".banner-slide");
-$prevBtn.addEventListener("click", () => {});
 
+// 배너 이미지 넣기
 function renderBanner(product) {
-    console.log(product);
     product.forEach((product, i) => {
         $bannerSlide[i].style.backgroundImage = `url("${product.image}")`;
+
+        $bannerSlide[i].addEventListener("click", () => {
+            location.href = `./product-details.html?id=${product.id}`;
+        });
     });
 }
 
-export { renderBanner };
+let intervalId; // 자동 슬라이드 멈추기 기능에 사용
+// 자동 슬라이드
+function intervalBanner() {
+    intervalId = setInterval(() => {
+        if (currentIndex !== $bannerSlide.length - 1) {
+            moveBanner(currentIndex + 1);
+        } else {
+            moveBanner(0);
+        }
+    }, 3000);
+}
+
+// 슬라이드 멈춤
+function stopInterval() {
+    clearInterval(intervalId);
+}
+
+// 배너에 마우스 오버 시 자동 슬라이드 멈춤
+$banner.addEventListener("mouseover", () => {
+    stopInterval();
+});
+
+// 배너에서 마우스 아웃 시 자동 슬라이드
+$banner.addEventListener("mouseout", () => {
+    intervalBanner();
+});
+
+let currentIndex = 0; // 현재 배너 위치 인덱스
+// 배너 슬라이드 적용 함수
+function moveBanner(num) {
+    $bannerSlides.style.transform = `translateX(${-num * 100}vw)`;
+    currentIndex = num;
+}
+
+// 이전 버튼 클릭 시 이전 배너로
+$prevBtn.addEventListener("click", () => {
+    if (currentIndex !== 0) {
+        moveBanner(currentIndex - 1);
+    }
+});
+
+// 다음 버튼 클릭 시 다음 배너로
+$nextBtn.addEventListener("click", () => {
+    if (currentIndex !== $bannerSlide.length - 1) {
+        moveBanner(currentIndex + 1);
+    }
+});
+
+export { renderBanner, intervalBanner };
